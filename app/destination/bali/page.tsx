8e47a928baf8558dc6e6ad75fd1ed14adbe37743
neha-kandari <kandarineha0508@gmail.com>
2025-09-17 21:18:37 +0530

    Update page.tsx

--- app/destination/bali/page.tsx
+++ app/destination/bali/page.tsx
@@ -3,6 +3,8 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
+import Head from 'next/head';
+import Script from 'next/script';
 import { formatDuration, extractNights } from '../../utils/durationFormatter';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
@@ -536,8 +538,36 @@ const BaliPage = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-900">
-      <Header />
+    <>
+      <Head>
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '517792703561205');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=517792703561205&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </Head>
+      <div className="min-h-screen bg-gray-900">
+        <Header />
       
       {/* Gallery Hero Section */}
       <section className="relative h-screen bg-gray-900 overflow-hidden">
@@ -1122,7 +1152,8 @@ const BaliPage = () => {
       </section>
 
       <Footer />
-    </div>
+      </div>
+    </>
   );
 };
 
